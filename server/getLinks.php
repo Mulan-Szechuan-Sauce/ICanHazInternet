f01d62eb0e45847610b41d78926b960cf067464f
Anthony Tam <anthony@anthonytam.me>
2017-02-28 19:47:14 -0500

    Uncompleted changes

--- server/getLinks.php
+++ server/getLinks.php
@@ -1,7 +1,15 @@
 <?php
 $postBody = file_get_contents("php://input");
 if($postBody != NULL) {
-    $conn = pg_connect();
+    $server = getenv("DB_SERVER");
+    $username = getenv("DB_USER");
+    $password = getenv("DB_PASS");
+    $database = getenv("DB_DATA");
+    $conn = pg_connect("host=$server dbname=$database user=$username password=$password");
+    if ($conn) {
+        $result = pg_prepare($conn, "insertURLs", "INSERT INTO (url) VALUES (?)");
+        if ($result) {
+            
     $urls = explode($_REQUEST['URLS'], '\n');
     for($urls as $url) {
          
