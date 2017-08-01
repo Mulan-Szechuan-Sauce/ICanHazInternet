9f6c4b475c646cbcd41e3114f730a7393e67072e
Josh Wolfe <joshuafwolfe@gmail.com>
2017-07-31 22:22:43 -0400

    Implemented server with bloom filter and db calls.

--- server/server.py
+++ server/server.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 import cgi
+import sqlite3
+from bloom_filter import BloomFilter
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-HOST = 'localhost'
+HOST = '192.168.0.104'
 PORT = 1337
+BLOOM = BloomFilter(10000)
+URLS_PER_CLIENT = 1
+TODO = ['google.com']
+DB = sqlite3.connect('icanhasdatabase').cursor()
 
 class ICanHasHandle(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(401)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
@@ -25,20 +31,32 @@ class ICanHasHandle(BaseHTTPRequestHandler):
             body = self.rfile.read(content_len).decode('utf8')
             self.siftUrls(body.splitlines())
 
+        print('Backlog Size: %d' % len(TODO))
         self.wfile.write(bytes('\n'.join(self.getUrls()), 'utf8'))
 
     def getUrls(self):
-        # TODO: Bloom it up.
-        return ['bloom it up']
+        urls = TODO[:URLS_PER_CLIENT]
+        del TODO[:URLS_PER_CLIENT]
+        return urls
 
     def siftUrls(self, urls):
-        # TODO: Bloom it up.
         for url in urls:
-            print(url)
+            if url in BLOOM:
+                DB.execute('SELECT * FROM urls WHERE url=?', [url])
+                if DB.fetchall == []:
+                    DB.execute('INSERT INTO urls (url) VALUES (?)', [url])
+                    BLOOM.add(url)
+                    TODO.append(url)
+            else:
+                DB.execute('INSERT INTO urls (url) VALUES (?)', [url])
+                BLOOM.add(url)
+                TODO.append(url)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     print('Getting all interwebs!')
     server_address = (HOST, PORT)
+    DB.execute('CREATE TABLE IF NOT EXISTS urls (url text primary key)')
     try:
         HTTPServer(server_address, ICanHasHandle).serve_forever()
     except KeyboardInterrupt:
